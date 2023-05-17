d110b35ca439e7627e927dc16d865d5f4bcc570b
phpcreativedb <phpdevdharmendra@gmail.com>
2023-05-17 11:29:00 +0530

    React Routes

--- src/components/Directory/derectory.component.jsx
+++ src/components/Directory/derectory.component.jsx
@@ -1,7 +1,6 @@
 import './derectory.styles.scss'
 import CategoryItem from '../category-item/category-item.component.jsx'
 const Derectory = (props) =>{
-    console.log(props);
     const {categories} = props;
     return (
         <div className='directory-container'>
