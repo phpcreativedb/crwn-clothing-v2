2220b1cf1acf18595eb9d84ac2c006f063d66e97
phpcreativedb <phpdevdharmendra@gmail.com>
2023-06-09 12:03:44 +0530

    firebase andfirestore added in firebase util file

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import {getAuth,signInWithRedirect,signInWithPopup,GoogleAuthProvider} from 'firebase/auth'
-
+import { getFirestore,doc,getDoc,setDoc } from 'firebase/firestore'
 const firebaseConfig = {
     apiKey: "AIzaSyDtj6ihE478x3Um6-wZmm6CbrwfKDUq3Ss",
     authDomain: "crwn-clothing-db-c0509.firebaseapp.com",
@@ -18,3 +18,22 @@ const firebaseConfig = {
 
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth,provider);
+  export const db = getFirestore();
+  export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db,'users',userAuth.uid);
+    const userSnapShot = await getDoc(userDocRef);
+    if(!userSnapShot.exists()){
+      const {displayName,email} = userAuth;
+      const createdAt = new Date();
+      try {
+        await setDoc(userDocRef,{
+          displayName,email,createdAt
+        })
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    
+    return userDocRef;
+    
+  }
